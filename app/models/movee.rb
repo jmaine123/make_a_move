9b4ee99da7040673e3aaf7aaa60f89b13e5d1737
Jermaine Smikle <jermaine.smikle@aol.com>
2018-09-26 16:55:48 -0400

    styling to homepage

--- app/models/movee.rb
+++ app/models/movee.rb
@@ -1,4 +1,8 @@
 class Movee < ApplicationRecord
   has_many :moving_events
-  has_many :movers
+  has_many :movers, through: :moving_events
+
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
 end
--- app/models/mover.rb
+++ app/models/mover.rb
@@ -1,4 +1,8 @@
 class Mover < ApplicationRecord
   belongs_to :moving_event, optional: true
-  has_many :movees 
+  has_many :movees, through: :moving_event
+
+  def full_name
+    self.first_name + ' '+ self.last_name
+  end
 end
