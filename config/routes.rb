61ae33acc80c5a9425a68809f4cce6874a77875d
Jermaine Smikle <jermaine.smikle@aol.com>
2018-09-27 15:51:54 -0400

    added session helper need to work sign up issues

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
     resources :moving_events
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   root 'pages#home'
 post '/movers/:mover_id/moving_events/new' => 'moving_events#create'
 end
